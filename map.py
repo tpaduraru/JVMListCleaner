1b6aa6fee20c03e01075a000a277449b0c28d6b3
DavPadur <164956758+DavPadur@users.noreply.github.com>
2025-01-07 11:48:22 -0800

    Update map.py
    improved dropdown code

--- map.py
+++ map.py
@@ -20,7 +20,41 @@ def map(file_path, dropdown_options):
         return
 
     # Populate dropdown_options with headers from the CSV
-    for idx, dropdown in enumerate(dropdown_options):
+    for dropdown in dropdown_options:
+        dropdown['values'] = headers
+        dropdown.set('')
+
+    # Keywords for each dropdown field
+    dropdown_keywords = {
+        0: ["first", "name"],         
+        1: ["last", "name"],    
+        2: ["email", "e-mail"],     
+        3: ["phone", "mobile"],        
+        4: ["address", "street"],   
+        5: ["city"],                  
+        6: ["state"],                  
+        7: ["zip"],                    
+        8: ["county"] 
+    }
+
+    # Iterate through headers and assign best matches to dropdowns
+    for idx, keywords in dropdown_keywords.items():
+        for header in headers:
+            h = header.lower()
+
+            # Check for keywords
+            if all(keyword in h for keyword in keywords): # for ANDed keywords
+                dropdown_options[idx].set(header) 
+                break
+            elif any(keyword in h for keyword in keywords): # for ORed keywords
+                dropdown_options[idx].set(header) 
+                break
+
+
+            
+
+
+'''    for idx, dropdown in enumerate(dropdown_options):
         dropdown['values'] = headers
         h = headers[idx].lower() # cleans
         dropdown.set('')
@@ -43,4 +77,13 @@ def map(file_path, dropdown_options):
        #try to map city
         elif ( h.find("city") ):
             dropdown_options[5].set(headers[idx])
+        # try to map state
+        elif ( h.find("state") ):
+            dropdown_options[5].set(headers[idx])
+        # try to map zip
+        elif ( h.find("zip") ):
+            dropdown_options[5].set(headers[idx])
+        # try to map county
+        elif ( h.find("county") ):
+            dropdown_options[5].set(headers[idx])'''
         
